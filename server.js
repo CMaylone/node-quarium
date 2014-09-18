02946b10f1ccb8ad1c0cfd3c6264417d205e1c48
Corey Maylone <cmaylone@gmail.com>
2014-09-17 22:54:29 -0400

    Renamed app.js to server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+
+var express = require('express'),
+    fs = require('fs'),
+    http = require('http'),
+    path = require('path'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    config = require('./config');
+
+var routePath = './routes/';
+var app = module.exports = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.set('port', process.env.PORT || config.port || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan(config.morganLogFormat));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
+
+// Load all routes in the routes folder.
+fs.readdirSync(routePath).forEach(function (file) {
+  var route = routePath + file;
+  require(route)(app);
+});
+
+// Set-up socket.io Communication
+io.sockets.on('connection', require('./routes/socket'));
+
+// Start the server.
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
