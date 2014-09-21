b801ec02d2b246b32abf3c19dacb1f98fc190001
Corey Maylone <cmaylone@gmail.com>
2014-09-21 18:34:36 -0400

    Added ability to mock temperature data so you can test on something that is not a Raspberry Pi. Fixed issue with live graphing.

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var config = {
   port: 3000, // Application port.
   morganLogFormat : 'dev', // The string pattern defining how morgan should log data.
-  aquariumTempProbeSerial: '000005507b51' // Serial Number of the device to read.
+  aquariumTempProbeSerial: '000005507b51', // Serial Number of the device to read.
+  mockTemperatureData: false // If true the /api/temperature endpoint will make fake temperature data.
 };
 
 module.exports = config;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,16 @@ var config = require('../config'),
 
 module.exports = function(app) {
   app.get('/api/temperature', function(req, res, next) {
-    temperatureProbe.read(config.aquariumTempProbeSerial, '*', function(err, temperature) {
-      if(err) return next(err);
+    if(!config.mockTemperatureData) {
+      temperatureProbe.read(config.aquariumTempProbeSerial, '*', function(err, temperature) {
+        if(err) return next(err);
 
+        res.set('content-type', 'application/json');
+        res.status(200).send(temperature);
+      });
+    } else {
       res.set('content-type', 'application/json');
-      res.status(200).send(temperature);
-    });
+      res.status(200).send({celsius:25, fahrenheit:Math.floor((Math.random() * 100) + 1)});
+    }
   });
 };
